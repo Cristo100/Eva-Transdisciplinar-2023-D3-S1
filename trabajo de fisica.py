5fb4cb5d5543f11356e45e48f3a4d190d7b737c2
Cristobal Pichara <135175850+Cristo100@users.noreply.github.com>
2023-06-06 16:26:01 -0400

    Codigo Plano Inclinado.py

--- trabajo de fisica.py
+++ trabajo de fisica.py
@@ -1,11 +1,23 @@
 import tkinter as tk
-ventana = tk.Tk() #ventana 
-ventana.geometry("800x600") #dimensiones de la ventana 
-ventana.configure(background= "medium sea green") #color de la ventana
-titulo = tk.Label (ventana,text = "trabajo de fisica", bg = "pink") #titulo del trabajo  
+
+#----------------------
+# Ventana
+#-----------------------
+# su creacion, dimensiones y color respectivamente.
+ventana = tk.Tk()
+ventana.geometry("800x600")
+ventana.configure(background= "medium sea green")
+
+#-------------------------
+# Comienzo del trabajo
+#-------------------------
+#titulo del trabajo  
+titulo = tk.Label (ventana,text = "trabajo de fisica", bg = "pink")
 titulo.pack(fill= tk.X)
+
 columna = tk.Canvas(ventana, width=200, height=600)
 columna.place(x=0, y=0)
 columna.create_rectangle(0, 0, 200, 800, fill='blue')
+
 ventana.resizable(False,False)
 ventana.mainloop()
